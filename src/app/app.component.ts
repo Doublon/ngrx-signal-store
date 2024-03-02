bd807245e35f42359b55a63b5bba8187ecb1ab59
Quentin Tusset <tusset.quentin@outlook.com>
2024-03-02 11:02:35 +0100

    Add loading status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TodosStore } from './store/todos.store';
 import { TodosListComponent } from "./component/todos-list/todos-list.component";
+import {MatSpinner} from '@angular/material/progress-spinner';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, TodosListComponent]
+    imports: [CommonModule, RouterOutlet, TodosListComponent, MatSpinner]
 })
 export class AppComponent implements OnInit {
 
